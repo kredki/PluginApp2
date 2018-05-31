7efa05c35e4762da7f6b6df8773865a816efdc63
TL <31960565+kredki@users.noreply.github.com>
2018-05-31 23:23:26 +0200

    add lng change

--- PluginApp/MainWindow.xaml.cs
+++ PluginApp/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Resources;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,11 +27,20 @@ namespace PluginApp
         //System.Windows.Ink.StrokeCollection _removed;
         System.Windows.Ink.StrokeCollection addedStrokes;
         System.Windows.Ink.StrokeCollection removedStrokes;
+        ResourceManager rm;
         private bool handle = true;
         List<PluginInterface> plugins = new List<PluginInterface>();
         public MainWindow()
         {
             InitializeComponent();
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("pl");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pl");
+            Assembly a = Assembly.Load("PluginApp");
+            rm = new ResourceManager("PluginApp.Resources", a);
+            colorBtn.Content = rm.GetString("Color");
+            uddoBtn.Content = rm.GetString("Undo");
+            redoBtn.Content = rm.GetString("Redo");
+            changeLngBtn.Content = rm.GetString("Changelanguage");
 
             String folderPath = @"L:\vcprojekty\PluginApp\plugins\netstandard2.0";
 
@@ -70,7 +82,7 @@ namespace PluginApp
         private void addMenuItems()
         {
             MenuItem mi1 = new MenuItem();
-            mi1.Header = "Black";
+            mi1.Header = rm.GetString("Black");
             mi1.Click += new RoutedEventHandler(mi_Click);
             ColorContexMenu.Items.Add(mi1);
 
@@ -87,7 +99,7 @@ namespace PluginApp
         {
             MenuItem m = (MenuItem)sender; // That's the sepcific item that has been clicked.
             String name = (sender as MenuItem).Header.ToString();
-            if (name.Equals("Black"))
+            if (name.Equals(rm.GetString("Black")))
             {
                 InkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
             } else
@@ -156,6 +168,33 @@ namespace PluginApp
             }
 
             InkCanvas.Strokes = addedStrokes;
+        }
+
+        private void ChangeLng(object sender, RoutedEventArgs e)
+        {
+            if(Thread.CurrentThread.CurrentUICulture.Equals(new CultureInfo("pl")))
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
+            } else
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("pl");
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("pl");
+            }
+            Assembly a = Assembly.Load("PluginApp");
+            rm = new ResourceManager("PluginApp.Resources", a);
+            colorBtn.Content = rm.GetString("Color");
+            uddoBtn.Content = rm.GetString("Undo");
+            redoBtn.Content = rm.GetString("Redo");
+            changeLngBtn.Content = rm.GetString("Changelanguage");
+
+            var menuItems = ColorContexMenu.Items.Cast<MenuItem>().ToArray();
+            foreach (MenuItem menuItem in menuItems)
+            {
+                menuItem.Click -= new RoutedEventHandler(mi_Click);
+            }
+            ColorContexMenu.Items.Clear();
+            addMenuItems();
         }
     }
 }
